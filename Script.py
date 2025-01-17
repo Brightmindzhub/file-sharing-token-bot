9f39e8dea911ecb86384108f939a8118cbf0b97c
LKDlovepreet <singhlovepreet05198@gmail.com>
2025-01-17 20:17:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ SUPPORT1 = "@ultroid0fficial_chat"
 OWNER_ID1 = 2016390875
 
 class script(object):
-    START_TXT = """<b>Hᴇʟʟᴏ {},\nᴍʏ ɴᴀᴍᴇ {},\n 【I'm Storage bot】\n+❌Don't send me messages directly I'm only File Share bot!/b>"""
+    START_TXT = """<b>Hᴇʟʟᴏ {},\nᴍʏ ɴᴀᴍᴇ {},\n 【I'm Storage bot】\n ❌Don't send me messages directly I'm only File Share bot!/b>"""
         
     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
