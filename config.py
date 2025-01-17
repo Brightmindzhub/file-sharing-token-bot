201ff2b70c0fa3e97b0cfdf7cb313ff9301ebd68
LKDlovepreet <singhlovepreet05198@gmail.com>
2025-01-17 17:38:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ OWNERID = int(environ.get("OWNERID" ,"2016390875"))
 YT = environ.get("YT", "https://youtube.com/@phdlust")
 
 # Bot Information
-API_ID = int(environ.get("API_ID", "23929647"))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "28923075"))
+API_HASH = environ.get("API_HASH", "77671b1fa735ceeeb4952da500db9124")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/0db18dde98ea3d0d4df0a.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6261595158').split()]
+BOT_USERNAME = environ.get("BOT_USERNAME", "videosstoragebot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "cloneuxb")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
+DB_URI = environ.get("DB_URI", "mongodb+srv://gangstertown5911:jLKUtc4BTfBQ75Lm@cluster0.clgw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "uxbfile")
 
 # Auto Delete Information
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002429236577"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "mdiskshortner.link") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "ead08e1329dbc1f5a2bd4dfe9e7c6e09b2c763bc") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/ultroid_official/18") # how to open link 
 
 # Website Info:
