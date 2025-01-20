a86e23a1305893f78a8d5e815f6d58199f66b920
LKDlovepreet <singhlovepreet05198@gmail.com>
2025-01-20 11:05:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "mdiskshortner.link") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "ead08e1329dbc1f5a2bd4dfe9e7c6e09b2c763bc") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "modijiurl.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "7035d9f382891e14d6d79dfe4f29e0174a36510f") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/ultroid_official/18") # how to open link 
 
 # Website Info:
