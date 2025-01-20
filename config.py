d7b201023284898566691cf199244cc10f12984b
LKDlovepreet <singhlovepreet05198@gmail.com>
2025-01-20 14:35:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "adrinolinks.in") # shortlink domain without https://
 SHORTLINK_API = environ.get("SHORTLINK_API", "2645d15d3244c88bc7e0f8f1d63fdfda5a57f85b") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/ultroid_official/18") # how to open link 
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Korean_adult_Japanese_chinese_ha/35") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
